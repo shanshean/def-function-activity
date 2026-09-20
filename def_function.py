49290c2ba1cb8d484d64c9571578a065cb60084a
shanshean <sheannenalerta@gmail.com>
2026-09-20 18:34:57 +0800

    Update def_function.py

--- def_function.py
+++ def_function.py
@@ -25,7 +25,7 @@ return balance
 #It is to calculate each payment and summing up the total rent.
 payment = []
 for month in ["August", "September", "October", "November", "December"]:
-	payment = float(input(f"Enter payment for {month}: ))
+	payment = float(input(f"Enter payment for {month}: "))
 	payments.append(payment)
 
 #Calling/invoking the def function with the collected rent balance
